da9991838d58d27072c30be37b72b54d902cd83b
dansoares8 <danielsoares8296@gmail.com>
2025-04-22 13:36:52 -0300

    new features, fix bugs and others.

--- models/Filters.php
+++ models/Filters.php
@@ -1,16 +1,20 @@
-<?php 
+<?php
 
-class Filters extends model {
+class Filters extends model
+{
 
-    
 
-    public function getFilters($filters){
+
+    public function getFilters($filters)
+    {
 
         $brands = new Brands();
         $products = new Products();
 
         $array = array(
             'brands' => array(),
+            'slider0' => 0,
+            'slider1' => 0,
             'maxslider' => 1000,
             'stars' => array(
                 '0' => 0,
@@ -24,34 +28,40 @@ class Filters extends model {
             'options' => array()
         );
 
-        
+
         $array['brands'] = $brands->getList();
         $brand_products = $products->getListOfBrands($filters);
 
         //Criando filtro de Marcas
-        foreach($array['brands'] as $bkey => $bitem){
+        foreach ($array['brands'] as $bkey => $bitem) {
 
             $array['brands'][$bkey]['count'] = '0';
-            
-            foreach($brand_products as $bproduct){
-                if($bproduct['id_brand'] == $bitem['id']){
+
+            foreach ($brand_products as $bproduct) {
+                if ($bproduct['id_brand'] == $bitem['id']) {
                     $array['brands'][$bkey]['count'] = $bproduct['c'];
                 }
             }
 
-            if($array['brands'][$bkey]['count'] == '0'){
+            if ($array['brands'][$bkey]['count'] == '0') {
                 unset($array['brands'][$bkey]);
             }
         }
 
         //Criando filtro de Preço:
+        if (isset($filters['slider0'])) {
+            $array['slider0'] = $filters['slider0'];
+        }
+        if (isset($filters['slider1'])) {
+            $array['slider1'] = $filters['slider1'];
+        }
         $array['maxslider'] = $products->getMaxPrice($filters);
 
         //Criando filtro das Estrelas:
         $star_products = $products->getListOfStars($filters);
-        foreach($array['stars'] as $skey => $sitem){
-            foreach($star_products as $sproduct){
-                if($sproduct['rating'] == $skey) {
+        foreach ($array['stars'] as $skey => $sitem) {
+            foreach ($star_products as $sproduct) {
+                if ($sproduct['rating'] == $skey) {
                     $array['stars'][$skey] = $sproduct['c'];
                 }
             }
